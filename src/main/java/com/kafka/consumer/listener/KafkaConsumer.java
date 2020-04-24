c3299c87e9452222c7c0c1de09ada55732c0c809
MamaFruma <52822814+MamaFruma@users.noreply.github.com>
2020-04-24 17:34:54 +0300

    Upgrade

---
+++ src/main/java/com/kafka/consumer/listener/KafkaConsumer.java
@@ -0,0 +1,22 @@
+package com.kafka.consumer.listener;
+
+import com.kafka.consumer.model.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics =  "topic_string", groupId = "consumer")
+    public void consumeString(String message) {
+        System.out.println("Received String message: " + message);
+    }
+
+    @KafkaListener(topics = "topic_json", groupId = "json_id", containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user) {
+        System.out.println(("Received Json message: " + user));
+    }
+
+}
+
---
+++ src/main/java/com/kafka/consumer/model/User.java
@@ -0,0 +1,26 @@
+package com.kafka.consumer.model;
+
+public class User {
+
+    private String name;
+    private String dept;
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+}
